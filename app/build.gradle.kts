68c7d358f46b4275933304dd0859870e32a95f88
tahereh <155296514+TaherehHasanpour@users.noreply.github.com>
2024-07-14 04:43:44 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,65 +68,67 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.activity:activity-compose:1.3.1")
 
-//
-//    //retrofit
-//    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-//
-//
-//    //hilt di
-//    implementation ("com.google.dagger:hilt-android:2.44")
-//    kapt ("com.google.dagger:hilt-compiler:2.44")
-//    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
-//
-//
-//    //compose navigation
-//    implementation ("androidx.navigation:navigation-compose:2.7.7")
-//
-//    //animation
-//    implementation ("com.airbnb.android:lottie-compose:5.2.0")
-//
-//    //coil - load image from url
-//    implementation ("io.coil-kt:coil-compose:2.2.2")
-//
-//    //swipe refresh
-//    implementation( "com.google.accompanist:accompanist-swiperefresh:0.30.0")
-//
-//    //system ui controller
-//    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
-//
-//    //datastore
-//    implementation ("androidx.datastore:datastore-preferences:1.1.0")
-//
-//    //pager
-//    implementation ("com.google.accompanist:accompanist-pager:0.29.0-alpha")
-//    implementation ("com.google.accompanist:accompanist-pager-indicators:0.29.0-alpha")
-//
-//    //room
-//    implementation ("androidx.room:room-runtime:2.6.1")
-//    kapt ("androidx.room:room-compiler:2.6.1")
-//    implementation ("androidx.room:room-ktx:2.6.1")
-//
-//    // paging
-//    implementation ("androidx.paging:paging-runtime-ktx:3.3.0-beta01")
-//    implementation ("androidx.paging:paging-compose:3.3.0-beta01")
-//
-//    // youtube player
-//    implementation("io.github.ilyapavlovskii:youtubeplayer-compose:2023.11.16")
-////    implementation("com.chrynan.uri:uri-core:0.3.3")
-////    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
-//
-//    // exo player
-////    implementation ("androidx.media3:media3-exoplayer:1.2.0")
-////    implementation ("androidx.media3:media3-exoplayer-dash:1.2.0")
-////    implementation ("androidx.media3:media3-ui:1.2.0")
-//
-//    // zoomable image
-////    implementation("me.saket.telephoto:zoomable:0.7.1")
-//    implementation("me.saket.telephoto:zoomable-image-coil:0.7.1")
-//
-//
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+
+
+
+//hilt di
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+
+    //compose navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
+    //animation
+    implementation ("com.airbnb.android:lottie-compose:5.2.0")
+
+    //coil - load image from url
+    implementation ("io.coil-kt:coil-compose:2.2.2")
+
+    //swipe refresh
+    implementation( "com.google.accompanist:accompanist-swiperefresh:0.30.0")
+
+    //system ui controller
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+
+    //datastore
+    implementation ("androidx.datastore:datastore-preferences:1.1.0")
+
+    //pager
+    implementation ("com.google.accompanist:accompanist-pager:0.29.0-alpha")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.29.0-alpha")
+
+    //room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // paging
+    implementation ("androidx.paging:paging-runtime-ktx:3.3.0-beta01")
+    implementation ("androidx.paging:paging-compose:3.3.0-beta01")
+
+    // youtube player
+    implementation("io.github.ilyapavlovskii:youtubeplayer-compose:2023.11.16")
+//    implementation("com.chrynan.uri:uri-core:0.3.3")
+//    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
+
+    // exo player
+//    implementation ("androidx.media3:media3-exoplayer:1.2.0")
+//    implementation ("androidx.media3:media3-exoplayer-dash:1.2.0")
+//    implementation ("androidx.media3:media3-ui:1.2.0")
+
+    // zoomable image
+//    implementation("me.saket.telephoto:zoomable:0.7.1")
+    implementation("me.saket.telephoto:zoomable-image-coil:0.7.1")
+
+
     //paging3
     implementation ("androidx.paging:paging-compose:3.3.0-beta01")
 
